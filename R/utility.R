ba648aa2c2c18944e09ed8dba5ec43fb191c7968
R. Wadhwa <raoulwadhwa@gmail.com>
2022-02-27 12:51:15 -0500

    documentation

--- R/utility.R
+++ R/utility.R
@@ -1,8 +1,29 @@
 #####DISCRETIZE#####
 # discretize fitness landscape values (numeric) into integer levels
-#' Discretize
+#' Discretize Numeric Variable Into Categories
 #'
+#' Takes a numeric variable (could be of class `numeric` or `integer`) and
+#' returns a discretized version, in which each element has been replaced by a
+#' single integer between `1` and `nlevels`, inclusive. The `nlevels` parameter
+#' is not included in the function definition, but is required and must be a
+#' positive integer value.
+#'
+#' @param x either a vector (`numeric` or `integer`) or `FitLandDF` object
+#' @return discretized form of `x`
 #' @export
+#' @examples
+#'
+#' ## discretize a numeric vector
+#' vec <- 1:10
+#' discretize(vec, nlevels = 5) # discretize into 5 categories
+#' discretize(vec, 2)           # discretize into 2 categories
+#'
+#' ## discretize a fitness landscape
+#' # create a 3x3x3 fitness landscape with values 1 through 27
+#' fl_data <- array(1:27, dim = rep(3, 3))
+#' my_fl <- fitscape::FitLandDF(fl_data)
+#' discretize(my_fl, nlevels = 2) # discretize landscape into 2 categories
+#' discretize(my_fl, 5)           # discretize landscape into 5 categories
 discretize <- function(x, ...) {
   UseMethod("discretize")
 }
