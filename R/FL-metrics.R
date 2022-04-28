67cac1e027af965ed213b460f0504fcf0c4eeef4
Raoul W <raoulwadhwa@gmail.com>
2022-04-28 11:53:59 -0400

    added metrics

--- R/FL-metrics.R
+++ R/FL-metrics.R
@@ -1,9 +1,9 @@
 #### All GLCM functions are applied to normalised co-occurrence matrices
 # These are square matrices of size n_levels x n_levels
 # where n_levels is the number of discretized fitness levels
-#' To generate a random co-occurrence matrix in a system with 3 levels
-#' x = matrix(sample.int(9, size = 9, replace = TRUE), ncol = 3)
-#' x = x / sum(x)
+# To generate a random co-occurrence matrix in a system with 3 levels
+# x = matrix(sample.int(9, size = 9, replace = TRUE), ncol = 3)
+# x = x / sum(x)
 
 #### P x-y ####
 # This is the marginal probability of specific differences between x and y
@@ -36,19 +36,6 @@ p_xplusy.FitLandDF <- function(x, axis, ...) {
     return(p_k)
   }
 
-##### CONTRAST #####
-# Contrast (also known as homogeneity) is the sum of the probability times the difference in neighboring values squared
-contrast.FitLandDF <- function(x, ...) {
-  nlevels=dim(x)[1]
-  c =  0
-  for (i in 1:nlevels){
-    for (j in 1:nlevels){
-      c = c+ abs(i-j)^2 * x[i,j]
-    }
-  }
-  return(c)
-}
-
 ##### CORRELATION #####
 # Correlation is the correlation between neighboring fitness values
 correlation.FitLandDF <- function(x, ...) {
@@ -64,43 +51,6 @@ correlation.FitLandDF <- function(x, ...) {
     }
   }
   return((corr/(sd_x*sd_y))[1,1])
-}
-
-
-##### ENTROPY #####
-# Entropy is the Shannon entropy
-entropy.FitLandDF <- function(x, ...) {
-  nlevels=dim(x)[1]
-  entropy =  - x * log(x)
-  return(sum(entropy, na.rm=TRUE))
-}
-
-
-#### INVERSE DIFFERENCE MOMENT #####
-# IDM
-idm.FitLandDF <- function(x, ...) {
-  nlevels=dim(x)[1]
-  idm =  0
-  for (i in 1:nlevels){
-    for (j in 1:nlevels){
-      idm = idm + 1/(1+ (i-j)^2) * x[i,j]
-    }
-  }
-  return(idm)
-}
-
-#### SUM OF SQUARES VARIANCE #####
-# ssv
-ssv.FitLandDF <- function(x, ...) {
-  nlevels=dim(x)[1]
-  mu = sum(x)/(nlevels**2)
-  ssv =  0
-  for (i in 1:nlevels){
-    for (j in 1:nlevels){
-      ssv = ssv + (i-mu)**2 * x[i,j]
-    }
-  }
-  return(ssv)
 }
 
 #### SUM AVERAGE #####
@@ -129,20 +79,6 @@ diff_entropy.FitLandDF <- function(x, ...) {
   return(sum(diff_entropy))
 }
 
-
-#### AUTOCORRELATION #####
-# Autocorrelation is the weighted sum of gray-level multiplications
-autocorr.FitLandDF <- function(x, ...) {
-  ac = 0
-  nlevels=dim(x)[1]
-  for (i in 1:nlevels){
-    for (j in 1:nlevels){
-      ac = ac + (i*j)*x[i,j]
-    }
-    }
-  return(ac)
-}
-
 #### DISSIMILARITY #####
 # Dissimilarity is the weighted sum of gray-level differences
 diss.FitLandDF <- function(x, ...) {
@@ -155,40 +91,3 @@ diss.FitLandDF <- function(x, ...) {
   }
   return(diss)
 }
-
-#### CLUSTER SHADE #####
-# Cluster shade is 3rd order of difference between i + j and mean i + j
-cs.FitLandDF <- function(x, ...) {
-  nlevels=dim(x)[1]
-  mu_x = weighted.mean(1:nlevels, rowSums(x))
-  mu_y = weighted.mean(1:nlevels, colSums(x))
-  cs = 0
-  nlevels=dim(x)[1]
-  for (i in 1:nlevels){
-    for (j in 1:nlevels){
-      cs = cs + (i+j-mu_x - mu_y)**3*x[i,j]
-    }
-  }
-  return(cs)
-}
-
-
-#### CLUSTER PROMINENCE #####
-# Cluster prominence is 4th order of difference between i + j and mean i + j
-cp.FitLandDF <- function(x, ...) {
-  nlevels=dim(x)[1]
-  mu_x = weighted.mean(1:nlevels, rowSums(x))
-  mu_y = weighted.mean(1:nlevels, colSums(x))
-  cp = 0
-  nlevels=dim(x)[1]
-  for (i in 1:nlevels){
-    for (j in 1:nlevels){
-      cp = cp + (i+j-mu_x - mu_y)**4*x[i,j]
-    }
-  }
-  return(cp)
-}
-
-
-
-
--- tests/testthat/test-metrics.R
+++ tests/testthat/test-metrics.R
@@ -1,17 +1,201 @@
-test_that("energy metric works", {
+# CHECKED that value doesn't change for different S3 generic formats
+# NOT CHECKED that actual value is correct for each metric
+
+#####AUTOCORRELATION#####
+test_that("autocorrelation metric works", {
   # create fitness landscape using FitLandDF object
-  vals <- runif(64)
-  vals <- array(vals, dim = rep(4, 3))
+  vals <- runif(27)
+  vals <- array(vals, dim = rep(3, 3))
   my_landscape <- fitscape::FitLandDF(vals)
 
-  # calculate energy of fitness landscape, assuming 2 discrete gray levels
+  # calculate metric for fitness landscape, assuming 2 discrete gray levels
+  a <- autocorrelation(my_landscape, nlevels = 2)
+
+  # extract normalized GLCM from fitness landscape
+  my_glcm <- get_comatrix(my_landscape, discrete = equal_discrete(2))
+
+  # calculate metric for extracted GLCM
+  b <- autocorrelation(my_glcm)
+
+  # test
+  expect_equal(a, b)
+})
+
+#####CLUSTER PROMINENCE#####
+test_that("cluster prominence metric works", {
+  # create fitness landscape using FitLandDF object
+  vals <- runif(27)
+  vals <- array(vals, dim = rep(3, 3))
+  my_landscape <- fitscape::FitLandDF(vals)
+
+  # calculate metric for fitness landscape, assuming 2 discrete gray levels
+  a <- cluster_prom(my_landscape, nlevels = 2)
+
+  # extract normalized GLCM from fitness landscape
+  my_glcm <- get_comatrix(my_landscape, discrete = equal_discrete(2))
+
+  # calculate metric for extracted GLCM
+  b <- cluster_prom(my_glcm)
+
+  # test
+  expect_equal(a, b)
+})
+
+#####CLUSTER SHADE#####
+test_that("cluster shade metric works", {
+  # create fitness landscape using FitLandDF object
+  vals <- runif(27)
+  vals <- array(vals, dim = rep(3, 3))
+  my_landscape <- fitscape::FitLandDF(vals)
+
+  # calculate metric for fitness landscape, assuming 2 discrete gray levels
+  a <- cluster_shade(my_landscape, nlevels = 2)
+
+  # extract normalized GLCM from fitness landscape
+  my_glcm <- get_comatrix(my_landscape, discrete = equal_discrete(2))
+
+  # calculate metric for extracted GLCM
+  b <- cluster_shade(my_glcm)
+
+  # test
+  expect_equal(a, b)
+})
+
+#####CONTRAST#####
+test_that("contrast metric works", {
+  # create fitness landscape using FitLandDF object
+  vals <- runif(27)
+  vals <- array(vals, dim = rep(3, 3))
+  my_landscape <- fitscape::FitLandDF(vals)
+
+  # calculate metric for fitness landscape, assuming 2 discrete gray levels
+  a <- contrast(my_landscape, nlevels = 2)
+
+  # extract normalized GLCM from fitness landscape
+  my_glcm <- get_comatrix(my_landscape, discrete = equal_discrete(2))
+
+  # calculate metric for extracted GLCM
+  b <- contrast(my_glcm)
+
+  # test
+  expect_equal(a, b)
+})
+
+#####ENERGY#####
+test_that("energy metric works", {
+  # create fitness landscape using FitLandDF object
+  vals <- runif(27)
+  vals <- array(vals, dim = rep(3, 3))
+  my_landscape <- fitscape::FitLandDF(vals)
+
+  # calculate metric for fitness landscape, assuming 2 discrete gray levels
   a <- energy(my_landscape, nlevels = 2)
 
   # extract normalized GLCM from fitness landscape
   my_glcm <- get_comatrix(my_landscape, discrete = equal_discrete(2))
 
-  # calculate energy of extracted GLCM
+  # calculate metric for extracted GLCM
   b <- energy(my_glcm)
+
+  # test
+  expect_equal(a, b)
+})
+
+#####ENTROPY#####
+test_that("entropy metric works", {
+  # create fitness landscape using FitLandDF object
+  vals <- runif(27)
+  vals <- array(vals, dim = rep(3, 3))
+  my_landscape <- fitscape::FitLandDF(vals)
+
+  # calculate metric for fitness landscape, assuming 2 discrete gray levels
+  a <- entropy(my_landscape, nlevels = 2)
+
+  # extract normalized GLCM from fitness landscape
+  my_glcm <- get_comatrix(my_landscape, discrete = equal_discrete(2))
+
+  # calculate metric for extracted GLCM
+  b <- entropy(my_glcm)
+
+  # test
+  expect_equal(a, b)
+})
+
+#####HOMOGENEITY#####
+test_that("homogeneity metric works", {
+  # create fitness landscape using FitLandDF object
+  vals <- runif(27)
+  vals <- array(vals, dim = rep(3, 3))
+  my_landscape <- fitscape::FitLandDF(vals)
+
+  # calculate metric for fitness landscape, assuming 2 discrete gray levels
+  a <- homogeneity(my_landscape, nlevels = 2)
+
+  # extract normalized GLCM from fitness landscape
+  my_glcm <- get_comatrix(my_landscape, discrete = equal_discrete(2))
+
+  # calculate metric for extracted GLCM
+  b <- homogeneity(my_glcm)
+
+  # test
+  expect_equal(a, b)
+})
+
+#####INVERSE DIFFERENCE#####
+test_that("inverse difference metric works", {
+  # create fitness landscape using FitLandDF object
+  vals <- runif(27)
+  vals <- array(vals, dim = rep(3, 3))
+  my_landscape <- fitscape::FitLandDF(vals)
+
+  # calculate metric for fitness landscape, assuming 2 discrete gray levels
+  a <- inv_diff(my_landscape, nlevels = 2)
+
+  # extract normalized GLCM from fitness landscape
+  my_glcm <- get_comatrix(my_landscape, discrete = equal_discrete(2))
+
+  # calculate metric for extracted GLCM
+  b <- inv_diff(my_glcm)
+
+  # test
+  expect_equal(a, b)
+})
+
+#####MAXIMUM PROBABILITY#####
+test_that("maximum probability metric works", {
+  # create fitness landscape using FitLandDF object
+  vals <- runif(27)
+  vals <- array(vals, dim = rep(3, 3))
+  my_landscape <- fitscape::FitLandDF(vals)
+
+  # calculate metric for fitness landscape, assuming 2 discrete gray levels
+  a <- max_prob(my_landscape, nlevels = 2)
+
+  # extract normalized GLCM from fitness landscape
+  my_glcm <- get_comatrix(my_landscape, discrete = equal_discrete(2))
+
+  # calculate metric for extracted GLCM
+  b <- max_prob(my_glcm)
+
+  # test
+  expect_equal(a, b)
+})
+
+#####SUM OF SQUARES#####
+test_that("sum of squares metric works", {
+  # create fitness landscape using FitLandDF object
+  vals <- runif(27)
+  vals <- array(vals, dim = rep(3, 3))
+  my_landscape <- fitscape::FitLandDF(vals)
+
+  # calculate metric for fitness landscape, assuming 2 discrete gray levels
+  a <- sum_squares(my_landscape, nlevels = 2)
+
+  # extract normalized GLCM from fitness landscape
+  my_glcm <- get_comatrix(my_landscape, discrete = equal_discrete(2))
+
+  # calculate metric for extracted GLCM
+  b <- sum_squares(my_glcm)
 
   # test
   expect_equal(a, b)
